9351b5718b0194a85c020ec96a4a5f13f0f2302f
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-01-11 13:44:04 +0800

    refactor

--- components/Iframe.tsx
+++ components/Iframe.tsx
@@ -4,9 +4,9 @@ import { IframeProps } from '../types/video';
 const Iframe = ({ width = 504, height = 336, url = '', title = '' }: IframeProps) => {
   return (
     <iframe
+      className="video-iframe"
       onLoadCapture={(onstart) => console.log({ onstart })}
       onLoad={(onload) => console.log({ onload })}
-      className="video-iframe"
       loading="lazy"
       frameBorder="0"
       title={title}
