d54a20e1f7b8e308632b65207d1b53752bce7f87
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-01-11 13:36:13 +0800

    create Iframe component for video

---
+++ components/Iframe.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { IframeProps } from '../types/video';
+
+const Iframe = ({ width = 504, height = 336, url = '', title = '' }: IframeProps) => {
+  return (
+    <iframe
+      onLoadCapture={(onstart) => console.log({ onstart })}
+      onLoad={(onload) => console.log({ onload })}
+      className="video-iframe"
+      loading="lazy"
+      frameBorder="0"
+      title={title}
+      allowFullScreen
+      width={width}
+      height={height}
+      src={url}
+      allow="accelerometer; autoplay; clipboard-write;
+  encrypted-media; gyroscope; picture-in-picture"
+    />
+  );
+};
+
+export default Iframe;
