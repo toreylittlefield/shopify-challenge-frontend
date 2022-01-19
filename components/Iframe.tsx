f715a3cfbe60ef0325f11a69e885fe6b00206bc2
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-01-20 06:49:19 +0800

    [edit] comment out logging

--- components/Iframe.tsx
+++ components/Iframe.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { IframeProps } from '../types/video';
 
-const Iframe = ({ width = 504, height = 336, url = '', title = '' }: IframeProps) => {
+const Iframe = ({ width = 504, height = 336, srcURL = '', title = '' }: IframeProps) => {
   return (
     <iframe
       className="video-iframe"
-      onLoadCapture={(onstart) => console.log({ onstart })}
-      onLoad={(onload) => console.log({ onload })}
+      // onLoadCapture={(onstart) => console.log({ onstart })}
+      // onLoad={(onload) => console.log({ onload })}
       loading="lazy"
       frameBorder="0"
       title={title}
       allowFullScreen
       width={width}
       height={height}
-      src={url}
+      src={srcURL}
       allow="accelerometer; autoplay; clipboard-write;
   encrypted-media; gyroscope; picture-in-picture; full-screen"
     />
