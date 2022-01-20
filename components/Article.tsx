0e85fb049541f85a72c3f3f589b83bcdc2be0863
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-01-20 10:05:49 +0800

    [add] setState prop for articles

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { SetStateAction } from 'react';
 import { Video } from './index';
 import Image from 'next/image';
 import { shimmer, toBase64 } from '../utils';
 import { CardButtons } from './CardButtons';
+import { UpdatedImgObj } from '../types/nasa-api-data';
+
+type PropType = UpdatedImgObj & {
+  index: number;
+  buttonType?: 'Like' | 'Delete';
+  addEntry?: (imgObj: UpdatedImgObj) => void;
+  deleteEntry?: (id: string) => void;
+  setArticles: (prev?: SetStateAction<any>) => void;
+};
 
 const Article = ({
   copyright = '',
-  date = new Date(),
+  earth_date = new Date().toLocaleDateString('en'),
   explanation = '',
   title = '',
   srcURL = 'https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png',
@@ -14,14 +23,13 @@ const Article = ({
   id = '',
   index = 2,
   ...rest
-}) => {
+}: PropType) => {
   const blurDataURL = `data:image/svg+xml;base64,${toBase64(shimmer('336', '504'))}`;
-  const { media_type, addEntry, deleteEntry } = rest;
-  console.log([rest]);
+
   return (
     <article className="article-container">
       <figure>
-        {media_type === 'video' ? (
+        {rest.media_type === 'video' ? (
           <Video key={id} {...{ title, srcURL, index, thumbnail_url }} />
         ) : (
           <Image
@@ -35,7 +43,8 @@ const Article = ({
           />
         )}
         <figcaption>
-          {title} :::: {date.toLocaleDateString('en', { dateStyle: 'medium' })}
+          {title} ::::
+          {earth_date}
         </figcaption>
         <p>{explanation}</p>
         <sub>{copyright}</sub>
@@ -43,9 +52,7 @@ const Article = ({
           id={id}
           title={title}
           srcURL={srcURL}
-          addEntry={addEntry}
-          deleteEntry={deleteEntry}
-          {...{ ...rest, copyright, explanation, thumbnail_url }}
+          {...{ ...rest, setArticles: rest.setArticles, earth_date, copyright, explanation, thumbnail_url }}
         />
       </figure>
     </article>
